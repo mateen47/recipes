08a262f3c4238c4cb9928b38ab53e21af379ad21
mateen47 <mateen.ismail17@gmail.com>
2020-07-17 10:22:57 -0400

    add edit and delete recipe method

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 	root 'pages#home'
 	get 'pages/home', to: 'pages#home'
     
-    get '/recipes', to:'recipes#index'
+    resources :recipes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

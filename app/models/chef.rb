c5f5748bb2a69853040b84b44042315f49b1397b
mateen47 <mateen.ismail17@gmail.com>
2020-07-17 06:37:30 -0400

    add index listing

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -3,4 +3,6 @@ class Chef < ApplicationRecord
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, length: {maximum: 255},
 									  format: {with: VALID_EMAIL_REGEX }
+
+	has_many :recipes
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_17_081906) do
+ActiveRecord::Schema.define(version: 2020_07_17_093522) do
 
   create_table "chefs", force: :cascade do |t|
     t.string "chefname"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_07_17_081906) do
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "chef_id"
   end
 
   create_table "users", force: :cascade do |t|
